26172e305fc9caf3ca5009a6a0e73435bbd9067a
Suds666 <joblesssuds@gmail.com>
2025-03-10 16:20:29 +0530

    updated cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, CC LAB 08\n";
   cout << "Hello, Jenkins\n";
-  cout << "I, Sudarshan Srinivasan, PES1UG22AM166 have successfully built and run this.\n";
+  cout << "I, Sudarshan Srinivasan, PES1UG22AM166 of Section C, have successfully built and run this.\n";
   return 0;
 }
